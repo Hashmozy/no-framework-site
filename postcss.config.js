41d1a3f200ade29b5a010dacf81571e342013463
Hashymozy <asherimusa505@gmail.com>
2024-02-05 10:12:00 +0300

    creating the project boilerplate

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+const postcssPresetEnv = require("postcss-preset-env");
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
+    plugins: [
+      postcssPresetEnv({
+        browsers: ["> 1%"],
+      }),
+      require("cssnano"),
+    ],
+  };
+  return;
+}
+
+module.exports = {};
---
+++ public/assets/scripts/bundle.js
@@ -0,0 +1,60 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scripts/main.js":
+/*!*****************************!*\
+  !*** ./src/scripts/main.js ***!
+  \*****************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: [BABEL] C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\src\\\\scripts\\\\main.js: @babel/helper-compilation-targets: 'browser' is not a valid target\\n- Did you mean 'node'? (While processing: \\\"C:\\\\\\\\Users\\\\\\\\Hashyl\\\\\\\\Documents\\\\\\\\vanilla-website\\\\\\\\node_modules\\\\\\\\@babel\\\\\\\\preset-env\\\\\\\\lib\\\\\\\\index.js\\\")\\n    at validateTargetNames (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\helper-compilation-targets\\\\lib\\\\index.js:60:13)\\n    at getTargets (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\helper-compilation-targets\\\\lib\\\\index.js:166:17)\\n    at getLocalTargets (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\preset-env\\\\lib\\\\index.js:189:48)\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\preset-env\\\\lib\\\\index.js:245:15\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\helper-plugin-utils\\\\lib\\\\index.js:33:12\\n    at async (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:35:33)\\n    at async (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:186:15)\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:216:13\\n    at Generator.next (<anonymous>)\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:180:21\\n    at Generator.next (<anonymous>)\\n    at Function.<anonymous> (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:21:3)\\n    at Generator.next (<anonymous>)\\n    at step (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:269:25)\\n    at evaluateAsync (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:291:5)\\n    at Function.errback (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:113:7)\\n    at errback (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:65:18)\\n    at async (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:188:17)\\n    at onFirstPause (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:216:13)\\n    at Generator.next (<anonymous>)\\n    at cachedFunction (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\caching.js:52:46)\\n    at cachedFunction.next (<anonymous>)\\n    at loadPresetDescriptor (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:292:43)\\n    at loadPresetDescriptor.next (<anonymous>)\\n    at recursePresetDescriptors (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:77:31)\\n    at recursePresetDescriptors.next (<anonymous>)\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:156:21\\n    at Generator.next (<anonymous>)\\n    at loadFullConfig (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:113:5)\\n    at loadFullConfig.next (<anonymous>)\\n    at transform (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transform.js:20:44)\\n    at transform.next (<anonymous>)\\n    at step (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:269:25)\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:223:11)\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:189:28\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:67:7\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:113:33\\n    at step (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:287:14)\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:223:11)\\n    at C:\\\\Users\\\\Hashyl\\\\Documents\\\\vanilla-website\\\\node_modules\\\\gensync\\\\index.js:37:40\");\n\n//# sourceURL=webpack://vanilla-website/./src/scripts/main.js?");
+
+/***/ }),
+
+/***/ "./src/styles/main.scss":
+/*!******************************!*\
+  !*** ./src/styles/main.scss ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://vanilla-website/./src/styles/main.scss?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	__webpack_modules__["./src/scripts/main.js"](0, {}, __webpack_require__);
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/styles/main.scss"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
